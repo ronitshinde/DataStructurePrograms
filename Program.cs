f60fbddec6515f7ca1676b048b55349489377689
ronitshinde <rony.shinde753@gmail.com>
2023-01-13 22:36:58 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructurePrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to data structure programs");
+        }
+    }
+}
