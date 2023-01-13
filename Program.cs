58b8a9ea063ee9ad7c99e48f825eb037b1a94e0e
ronitshinde <rony.shinde753@gmail.com>
2023-01-13 22:44:36 +0530

    Added bubble sort method

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            InsertionSort insertionSort = new InsertionSort();
-            insertionSort.DisplayInsertionSort();
+            BubbleSort bubbleSort = new BubbleSort();
+            bubbleSort.DisplayBubbleSort();
         }
     }
 }
