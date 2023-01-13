dff2e6d62ae7cf8164b24aee10b6bde73bf48d1b
ronitshinde <rony.shinde753@gmail.com>
2023-01-13 22:42:01 +0530

    Added code for insertion sort

---
+++ InsertionSort.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace DataStructurePrograms
+{
+    public class InsertionSort
+    {
+        public void DisplayInsertionSort()
+        {
+            int[] arr=new int[5] {23, 45, 12, 74, 56};
+            int n = 5, i, j, val, flag;
+
+            Console.WriteLine("Insertion Sort");
+            Console.WriteLine("Initial array is :");
+            for (i=0; i<n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            for (i=1; i<n; i++)
+            {
+                val = arr[i];
+                flag = 0;
+                for (j=i-1;j>=0 && flag!=1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else
+                    {
+                        flag = 1;
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Sorted array is : ");
+            for(i = 0; i<n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
